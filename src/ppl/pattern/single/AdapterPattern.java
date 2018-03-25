aeb254d0d37d0c335e6b5ae975496214eb07e258
ppl <a120209520@icloud.com>
2018-03-25 19:58:05 +0800

    [AdapterPattern]

---
+++ src/ppl/pattern/single/AdapterPattern.java
@@ -0,0 +1,50 @@
+package ppl.pattern.single;
+/**
+ * 适配器模式：
+ * 使用适配器来对两端接口进行适配
+ * 常用于新老系统更新适配
+ * 再例如InputStreamReader(InputStream)也用到了适配器模式
+ * @author Smith
+ *
+ */
+public class AdapterPattern
+{
+	public static void main(String[] args)
+	{
+		KeyBoard kb = new KeyBoard();
+		ScreenPrintAdapter adapter = new ScreenPrintAdapter();
+		kb.typeWriting("adapte me adapte me!", adapter);
+	}
+}
+
+class KeyBoard
+{
+	public void typeWriting(String s, PrintAdapter pa)
+	{
+		pa.output(s);
+	}
+}
+
+class Screen
+{
+	public void print(String s)
+	{
+		System.out.println(s);
+	}
+}
+
+interface PrintAdapter
+{
+	void output(String s);
+}
+
+class ScreenPrintAdapter implements PrintAdapter
+{
+	private Screen sc;
+	@Override
+	public void output(String s)
+	{
+		sc = new Screen();
+		sc.print(s);
+	}
+}
